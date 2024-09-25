26d5fb31644349a4145e8c5217e06adc20c713e5
yasarcandamli <yasarcandamli@gmail.com>
2024-09-25 18:29:25 +0300

    Örnek Proje - Hata Düzeltmeleri ve JavaScript'e Giriş

--- 07-Week07/js/main.js
+++ 07-Week07/js/main.js
@@ -11,8 +11,12 @@
         }
     });
 
+    var glightbox = GLightbox({
+        selector: '.glightbox',
+    });
+
     var photos = GLightbox({
-        selector: '#photos .photo'
+        selector: '#photos .photo',
     });
 
     var comment = new Swiper ('#comments .swiper', {
@@ -22,4 +26,50 @@
             type: 'bullets',
         }
     });
+
+
+    var header = document.getElementById('header');
+
+    var headerScrolled = function() {
+        if (window.scrollY > 100) {
+            header.classList.add('header-scrolled');
+        } else {
+            header.classList.remove('header-scrolled');
+        }
+    }
+
+    window.addEventListener('load', headerScrolled);
+    document.addEventListener('scroll', headerScrolled);
+
+    var links = document.getElementsByClassName('scrollto');
+    var focusSectionLink = function(event) {
+        for (const link of links) {
+            var id = link.hash.slice(1);
+            var section = document.getElementById(id);
+            var position = window.scrollY + (window.innerHeight/2);
+
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                link.ariaCurrent = 'page';
+                link.classList.add('active');
+            } else {
+                link.ariaCurrent = null;
+                link.classList.remove('active');
+            }
+        }
+    }
+    var focusSection = function(event) {
+        event.preventDefault();
+        var id = event.target.hash.slice(1);
+        var section = document.getElementById(id);
+        if (section) {
+            window.scrollTo({
+                top: section.offsetTop - header.offsetHeight,
+                behavior: 'smooth',
+            })
+        }
+    }
+    window.addEventListener('scroll', focusSectionLink)
+    for (const link of links) {
+        link.addEventListener('click', focusSection);
+    }
 })();
