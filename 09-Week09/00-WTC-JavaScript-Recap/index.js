7e6fda22c8747abb5e81e368f3a0826a13deb682
yasarcandamli <yasarcandamli@gmail.com>
2024-10-11 15:41:05 +0300

    Haftalık Teknik İçerik - JavaScript Recap - Temel JavaScript 2

--- 09-Week09/00-WTC-JavaScript-Recap/index.js
+++ 09-Week09/00-WTC-JavaScript-Recap/index.js
@@ -1,4 +1,4 @@
-// let const var
+// DEĞİŞKENLER - let const var
 
 // let personName = "Yaşar Can";
 // let greetingTemplate = `Hello ${personName}!`;
@@ -8,7 +8,40 @@
 // const personName = "Yaşar Can";
 // personName = "Muhammed"; // const'ta yeni değer atayamıyoruz!
 
-var personName = "Yaşar Can";
-personName = "Muhammed"; // var'da yeni değer atayabiliyoruz!
+// var personName = "Yaşar Can";
+// personName = "Muhammed"; // var'da yeni değer atayabiliyoruz!
 
-console.log(personName);
+// console.log(personName);
+
+// ------------------------------------------------------------------
+
+// SCOPE
+// const ve let block scope'da etkindir.
+// var personName2 = "Mehmet"
+// function main() {
+//     // var personName2 = "Ahmet"
+//     {
+//         var personName2 = "Hakan";
+//         console.log(personName2); //Hakan
+//         {
+//         }
+//     }
+//     console.log(personName2); //Hakan
+// }
+// main();
+// console.log(personName2); //Mehmet
+
+// IIFE (modülün ilkel hali)
+const $ = (function jQuery() {
+    var size = 800;
+    function ajax() {
+        console.log("ajax2222");
+    }
+
+    return {
+        size: size,
+        ajax: ajax
+    }
+})()
+
+$.ajax();
