92f5a473f0c2f7047111d1c53030056852645ecd
yasarcandamli <yasarcandamli@gmail.com>
2024-10-11 12:26:08 +0300

    Haftalık Teknik İçerik - JavaScript Recap - Temel JavaScript 1

---
+++ 09-Week09/00-WTC-JavaScript-Recap/index.js
@@ -0,0 +1,14 @@
+// let const var
+
+// let personName = "Yaşar Can";
+// let greetingTemplate = `Hello ${personName}!`;
+// let concatnation = "Hello " + personName + "!"
+// personName = "Muhammed"; // let'te yeni değer atayabiliyoruz!
+
+// const personName = "Yaşar Can";
+// personName = "Muhammed"; // const'ta yeni değer atayamıyoruz!
+
+var personName = "Yaşar Can";
+personName = "Muhammed"; // var'da yeni değer atayabiliyoruz!
+
+console.log(personName);
