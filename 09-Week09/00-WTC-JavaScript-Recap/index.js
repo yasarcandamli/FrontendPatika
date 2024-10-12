635f847812909f8fb9261702aa34ccb4c76f8cef
yasarcandamli <yasarcandamli@gmail.com>
2024-10-12 10:30:58 +0300

    Haftalık Teknik İçerik - JavaScript Recap - Temel JavaScript 6

--- 09-Week09/00-WTC-JavaScript-Recap/index.js
+++ 09-Week09/00-WTC-JavaScript-Recap/index.js
@@ -50,7 +50,7 @@
 
 // DATA TYPES
 // string, number, boolean, undefined => PRIMITIVE TYPES
-// null, object, function => REFERENCE TYPES - NON PRIMITIVE
+// object, function => REFERENCE TYPES - NON PRIMITIVE
 
 // let personName = "Yaşar Can";
 // let personAge = 26;
@@ -60,17 +60,131 @@
 
 // console.log(personName);
 
-const person = {
-    name: "Yaşar Can",
-    age: 26,
-    isOverAge: true,
-}; // adresi tutar
+// const person = {
+//     name: "Yaşar Can",
+//     age: 26,
+//     isOverAge: true,
+//     notes: ["note1", "note2"],
+//     notesObj: {
+//         0: "note1",
+//         1: "note2",
+//         length: 2
+//     }
+// }; // adresi tutar
 
-const person2 = person; // aynı adrese eşitler
+// const person2 = person; // aynı adrese eşitler
 
-person2.name = "Emin"; // aynı adresin özelliğini değiştirir
-
-
-console.log(person.name); // aynı adresin özelliği değişmişti
+// person2.name = "Emin"; // aynı adresin özelliğini değiştirir
 
 
+// console.log(Array.isArray(person.notes));
+// console.log(Array.isArray(person.notesObj));
+
+// ------------------------------------------------------------------
+
+// FUNCTION DEFINING
+
+// function f1(arg1, arg2) {
+//     console.log('f1', arg1, arg2);
+// }
+
+// const f2 = function (arg1, arg2) {
+//     console.log('f2', arg1, arg2);
+// }
+
+// const f3 = (arg1, arg2) => {
+//     console.log('f3', arg1, arg2);
+// }
+
+// f1(1, 2);
+// f2(3, 4);
+// f3(5, 6);
+
+// ASYNC
+// console.log("ilk sıradaki işlem");
+// setTimeout(() => {
+//     console.log("ikinci sıradaki işlem");
+// }, 3000)
+// console.log("üçüncü sıradaki işlem");
+
+// const wait = (ms) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve()
+//         }, ms);
+//     })
+// }
+
+// async function main() {
+//     console.log("ilk sıradaki işlem");
+//     await wait(2000)
+//     console.log("ikinci sıradaki işlem");
+//     await wait(2000)
+//     console.log("üçüncü sıradaki işlem");
+// }
+
+// main();
+
+// ------------------------------------------------------------------
+
+// KOŞULLU İFADELER
+
+// const person = {
+//     name: "Johnn Doe",
+//     age: 15,
+//     location: "USA",
+// }
+
+// const isOverAge = (person) => person.age > 18;
+
+// if (isOverAge(person)) {
+//     console.log("Hello World");
+// }
+
+// DÖNGÜLER
+
+// const wait = (ms) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve()
+//         }, ms);
+//     })
+// }
+
+// async function main() {
+//     for (let i = 0; i < 10; i++) {
+//         console.log("Yaşar Can", i);
+//         await wait(500);
+//     }
+
+//     let i = 0;
+//     while (i < 10) {
+//         console.log("Yaşar Can");
+//         i++;
+//         await wait(2000);
+//     }
+// }
+// main();
+
+// OPERATÖRLER
+// assign operation
+
+// const a = 5;
+// const b = 10;
+// let j = 0;
+// let i = 0;
+// let k = (i = a + b);
+// console.log(k);
+// console.log(i);
+
+// less than
+// greater than
+// less than or equal to
+// greater than or equal to
+// increase by 1
+// decrease by 1
+// sum +
+// concat +
+// difference -
+
+// ------------------------------------------------------------------
